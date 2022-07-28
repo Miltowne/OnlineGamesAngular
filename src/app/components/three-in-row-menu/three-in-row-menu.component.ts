b0c8c94f1b92d369608bd2f0313a32bede37015b
Emil Uhlin <emil.uhlin@se.experis.com>
2022-07-28 15:39:28 +0200

    fix: added missing files

---
+++ src/app/components/three-in-row-menu/three-in-row-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-three-in-row-menu',
+  templateUrl: './three-in-row-menu.component.html',
+  styleUrls: ['./three-in-row-menu.component.css']
+})
+export class ThreeInRowMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
