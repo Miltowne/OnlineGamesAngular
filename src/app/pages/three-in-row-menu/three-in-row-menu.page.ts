4bff911c1418c837e0a32336cddcc90294dd973e
Emil Uhlin <emil.uhlin@se.experis.com>
2022-07-29 13:14:41 +0200

    fix: commit missing files

---
+++ src/app/pages/three-in-row-menu/three-in-row-menu.page.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ThreeInRowService } from 'src/app/services/three-in-row.service';
+import { UserService } from 'src/app/services/user.service';
+import { ThreeInRowSession } from 'src/models/three-in-row.game.model';
+import { User } from 'src/models/user.model';
+
+@Component({
+  selector: 'app-three-in-row-menu',
+  templateUrl: './three-in-row-menu.page.html',
+  styleUrls: ['./three-in-row-menu.page.css']
+})
+export class ThreeInRowMenuPage {
+
+  get user(): User {
+    if (!this.userService.user)
+      throw new Error("User required")
+      
+    return this.userService.user
+  }
+
+  get inSession(): boolean {
+    return this.service.inSession
+  }
+
+  get gameSessions(): ThreeInRowSession [] {
+    if (!this.userService.user)
+      throw new Error("There must be a user!")
+
+    return this.service.getPlayerSessions(this.userService.user.username)
+  }
+
+  constructor(private readonly service: ThreeInRowService, private readonly userService: UserService) { }
+
+  ngOnInit(): void {
+    this.service.getAllSessions()
+  }
+
+}
---
+++ src/app/pages/three-in-row/three-in-row-game.page.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-three-in-row-game',
+  templateUrl: './three-in-row-game.page.html',
+  styleUrls: ['./three-in-row-game.page.css']
+})
+export class ThreeInRowGamePage {
+
+  constructor() { }
+}
